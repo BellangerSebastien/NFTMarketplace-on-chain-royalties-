3c2476263d0063acd4b812a68eec3b0733b83e44
mhuryanov <mhuryanov@gmail.com>
2022-04-01 14:53:31 +0200

    init

---
+++ contracts/BookNFT.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+
+contract BookNFT is ERC1155{
+    
+}
---
+++ contracts/PostNFT.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+
+contract PostNFT is Ownable, ERC721, Pausable {
+    constructor(){
+        
+    }
+    
+}
